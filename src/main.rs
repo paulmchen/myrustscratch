6da4874ce3f59858c3205666dd34666602fea4e1
Paul Chen <paul.chen1@huawei.com>
2021-02-24 17:18:10 -0500

    update examples

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,23 @@
-struct User {
-    name: String,
-    email: String,
-    sign_in_count: u64,
-    active: bool,
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
-fn build_user(email: String, name: String) -> User {
-    User {
-        name,
-        email,
-        sign_in_count: 1,
-        active: true,
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
     }
 }
 
-struct Color (i32,i32,i32);
-struct Point (i32,i32,i32);
-
 fn main() {
-    let user1 = build_user(String::from("paulc@abc.com"), String::from("paulc"));
-    println!("User: {} has email {}.", user1.name, user1.email);
-
-    let user2 = User {
-        email: String::from("jk@hotmail.com"),
-        name: String::from("jk"),
-        ..user1
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
     };
 
-    println!("User2 has activate status of {}.", user2.active);
-    let black = Color(0,0,0);
-    let point = Point(1,2,3);
-    println!("point x:{}, y:{}, z:{}.", point.0, point.1, point.2);
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
 }
